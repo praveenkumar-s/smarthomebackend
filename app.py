4685c9f9f86ed990a6ea4b57150f0c6f00b8e98d
praveenkumar-s <praveenkumar.s@realimage.com>
2018-06-05 10:41:02 +0530

    added runtime

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def getdata():
 if __name__ == '__main__':
     from os import environ
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False , host='0.0.0.0', port=port)
+    app.run(debug=False , host='0.0.0.0', port=port, threaded =True)
