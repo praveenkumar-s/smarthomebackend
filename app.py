aafefecf77570cb06c5521f27312be8d8a830739
praveenkumar-s <praveenkumar.s@realimage.com>
2018-06-05 10:20:56 +0530

    changes

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def getdata():
 
 if __name__ == '__main__':
     from os import environ
-    
-    app.run(debug=False , host='0.0.0.0', port=5000 , threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False , host='0.0.0.0', port=port)
