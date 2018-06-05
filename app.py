cdc941850fa72f8d8ac7caa02526262a1ee28d13
praveenkumar-s <praveenkumar.s@realimage.com>
2018-06-05 10:16:57 +0530

    changed port

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def getdata():
 if __name__ == '__main__':
     from os import environ
     
-    app.run(debug=False , host='0.0.0.0', port=environ.get("PORT", 5000) , threaded=True)
+    app.run(debug=False , host='0.0.0.0', port=5000 , threaded=True)
